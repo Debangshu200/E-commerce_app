3bd393e55f315932dc38a9141566ff1f8bc3e9ac
Cypher <debangshudas200@gmail.com>
2024-02-27 17:09:28 +0530

    final code
    Ecommerce MERN stack app

---
+++ routes/productRoutes.js
@@ -0,0 +1,78 @@
+import express from "express";
+import {
+  brainTreePaymentController,
+  braintreeTokenController,
+  createProductController,
+  deleteProductController,
+  getProductController,
+  getSingleProductController,
+  productCategoryController,
+  productCountController,
+  productFiltersController,
+  productListController,
+  productPhotoController,
+  relatedProductController,
+  searchProductController,
+  updateProductController,
+} from "../controller/productController.js";
+import { requireSignIn, isAdmin } from "../middlewares/authMiddleware.js";
+import formidable from "express-formidable";
+
+const router = express.Router();
+
+//routes
+router.post(
+  "/create-product", //create product
+  requireSignIn,
+  isAdmin,
+  formidable(),
+  createProductController
+);
+
+//routes
+router.put(
+  "/update-product/:pid", // update product
+  requireSignIn,
+  isAdmin,
+  formidable(),
+  updateProductController
+);
+
+//get products
+router.get("/get-product", getProductController);
+
+//single product
+router.get("/get-product/:slug", getSingleProductController);
+
+//get photo
+router.get("/product-photo/:pid", productPhotoController);
+
+//delete product
+router.delete("/delete-product/:pid", deleteProductController);
+
+//filter product
+router.post("/product-filters", productFiltersController);
+
+//count product
+router.get("/product-count", productCountController);
+
+//product per page
+router.get("/product-list/:page", productListController);
+
+//search product
+router.get("/search/:keyword", searchProductController);
+
+//similar product
+router.get("/related-product/:pid/:cid", relatedProductController);
+
+//category wise product
+router.get("/product-category/:slug", productCategoryController);
+
+// payment routes
+//token
+router.get("/braintree/token", braintreeTokenController);
+
+//payments
+router.post("/braintree/payment", requireSignIn, brainTreePaymentController);
+
+export default router;
